ea1312139e1664d6810dac0e9ef7f4a4778d91c1
SNOWNUMB <mamayev.stas@gmail.com>
2025-04-06 15:20:26 +0500

    UpdateFile

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
     path('', views.home, name='home'),
 ]
